e0a0b4da89c89a54c50adddde38e7623de5cd251
Nicolas Blanco <nicolas@nicolasblanco.fr>
2016-10-20 14:01:03 +0200

    Stub ARGV

--- spec/parking_operator_client_spec.rb
+++ spec/parking_operator_client_spec.rb
@@ -3,6 +3,7 @@ require 'parking_operator_client'
 RSpec.describe ParkingOperatorClient do
   describe '#initialize' do
     it 'sends commands to the operator' do
+      expect(ARGV).to receive(:any?).and_return(false)
       expect(Kernel).to receive(:loop).and_yield
       expect($stdin).to receive(:gets).and_return("my_command\n")
       expect_any_instance_of(ParkingOperator).to receive(:execute_command).with('my_command').and_return('OK')
